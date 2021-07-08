50682a3473bd93a8699c60bb0b399edf3e138a48
Lucas Magalhães <lucasgsm88@gmail.com>
2021-07-08 17:34:44 -0300

    heroku

--- frontend/src/api/UserApi.ts
+++ frontend/src/api/UserApi.ts
@@ -2,42 +2,22 @@ import axios from "axios";
 import { UserDetailDto, UserRepositoryDto, UserResumeDto } from "../models";
 
 export class UserApi {
+  constructor() {
+    axios.defaults.baseURL = "http://localhost:3100/api";
+  }
+
   async fetch(since?: number) {
-    const response = await axios.get<UserResumeDto[]>(
-      "http://localhost:3100/api/users",
-      {
-        params: {
-          since,
-        },
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-        },
-      }
-    );
+    const response = await axios.get<UserResumeDto[]>("users");
     return response.data;
   }
 
   async fetchDetail(userName: string) {
-    const response = await axios.get<UserDetailDto>(
-      `http://localhost:3100/api/users/${userName}/detail`,
-      {
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-        },
-      }
-    );
+    const response = await axios.get<UserDetailDto>(`${userName}/detail`);
     return response.data;
   }
 
   async fetchRepositories(userName: string) {
-    const response = await axios.get<UserRepositoryDto[]>(
-      `http://localhost:3100/api/users/${userName}/repos`,
-      {
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-        },
-      }
-    );
+    const response = await axios.get<UserRepositoryDto[]>(`${userName}/repos`);
     return response.data;
   }
 }
