db77b333a7d59c491a6f16e152408ff6bd975dc3
Lucas Magalhães <lucasgsm88@gmail.com>
2021-07-08 18:42:03 -0300

    heroku

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -17,6 +17,6 @@ async function bootstrap() {
   );
   app.enableCors();
   console.log(process.env.TOKEN_GITHUB, process.env.PORT);
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000, "0.0.0.0");
 }
 bootstrap();
